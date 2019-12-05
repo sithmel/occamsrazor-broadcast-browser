043ddc91d5fb76097b3761ba883d1d5a3332e0f0
sithmel <maurizio.lupo@gmail.com>
2019-12-05 10:57:01 +0000

    update packages, remove lock

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-function addBroadcastMethod(events, w, localStorage_key) {
-  var windowId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-  w = w || window;
-  localStorage_key = localStorage_key || '__or_event';
+function addBroadcastMethod (events, w, localStorageKey) {
+  var windowId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
+  w = w || window
+  localStorageKey = localStorageKey || '__or_event'
 
-  events.broadcast = function broadcast() {
-    var args = Array.prototype.slice.call(arguments, 0);
+  events.broadcast = function broadcast () {
+    var args = Array.prototype.slice.call(arguments, 0)
     // adding a timestamp to ensure every message is different
-    var obj = { args: args, ts: Date.now(), windowId: windowId };
-    var json = JSON.stringify(obj);
-    w.localStorage.setItem(localStorage_key, json);
-  };
+    var obj = { args: args, ts: Date.now(), windowId: windowId }
+    var json = JSON.stringify(obj)
+    w.localStorage.setItem(localStorageKey, json)
+  }
 
   w.addEventListener('storage', function (e) {
-    var obj, args;
-    if (e.key === localStorage_key) {
-      obj = JSON.parse(e.newValue);
-      args = obj.args;
+    var obj, args
+    if (e.key === localStorageKey) {
+      obj = JSON.parse(e.newValue)
+      args = obj.args
       if (obj.windowId !== windowId) {
         // this prevents IE9-11 buggy behaviour triggering
         // event on the same window
-        events.trigger.apply(null, args);
+        events.trigger.apply(null, args)
       }
     }
-  });
+  })
 
-  return events;
+  return events
 }
 
-module.exports = addBroadcastMethod;
+module.exports = addBroadcastMethod
